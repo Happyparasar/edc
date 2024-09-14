72bf8027b0202f86844f95b778a60c2c6342bbe3
happy7970 <happyparasar752@gmail.com>
2024-09-14 10:21:56 +0530

    updation

---
+++ index.js
@@ -0,0 +1,28 @@
+/*const express = require('express');
+const app = express();
+const user = require('./routes/user');
+app.use(user);
+
+
+
+app.listen(3000,(err)=>{
+    if(err){
+        console.log(err)
+    } else {
+        console.log('Server is running on port 3000')
+    }
+}) */
+const express = require('express');
+const app = express();
+const user = require('./routes/user');
+app.use(user);
+
+
+
+app.listen(3000, (err) => {
+    if (err) {
+        console.log(err)
+    } else {
+        console.log("server is running on 3000")
+    }
+})
